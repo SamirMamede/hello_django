ff64c76914c69c6a0fe9f4baccce7598adc8d651
SamirMamede <anuarsamir@gmail.com>
2022-01-13 02:50:33 -0300

    Exercícios para praticar, criando rotas para subtração e multiplicação.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def soma(requests, num1, num2):
+     resultado = (int(num1) + int(num2))
+     return HttpResponse('<h1>Soma de {} e {} é igual à {} </h1>'.format(num1, num2, resultado))
 
-def hello(requests, nome, idade):
-    return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+def subtracao(requests, num1, num2):
+    resultado = (int(num1) - int(num2))
+    return HttpResponse('<h1>Subtração de {} com {} é igual à {} </h1>'.format(num1, num2, resultado))
+
+def multiplicacao(requests, num1, num2):
+    resultado = (int(num1) * int(num2))
+    return HttpResponse('<h1>Multiplicação de {} e {} é igual à {} </h1>'.format(num1, num2, resultado))
